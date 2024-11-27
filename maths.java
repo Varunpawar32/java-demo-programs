fe0490ed8aa67acf06daed9b923070fdd0218ea8
Varunpawar32 <138917299+Varunpawar32@users.noreply.github.com>
2024-11-27 19:28:51 +0530

    Add files via upload

---
+++ maths.java
@@ -0,0 +1,18 @@
+public class maths 
+{
+public static void main(String[] args)
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = a+b+c;
+	int e = c-b-a;
+	int f = c*b*a;
+	int g = c*(b-a);
+
+	System.out.println("addition of a , b, c " + d);
+	System.out.println("sub = "+e);
+	System.out.println("mul = "+f);
+	System.out.println("areth = "+g);
+}
+} 
